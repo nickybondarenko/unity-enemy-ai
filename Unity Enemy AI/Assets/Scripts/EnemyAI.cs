c31593c8544cec03279ba806d2f68bcceac63d14
vbondarenko1 <vbondarenko@babbel.com>
2020-04-28 19:57:08 +0200

    first navmesh baked

---
+++ Unity Enemy AI/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh component is not attached to " + gameObject.name);
+        }
+        else {
+            SetDestination();
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (_destination != null) {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
