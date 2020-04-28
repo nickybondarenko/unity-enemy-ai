88c5e0a384db30a8a7cb1eb380b622c1003fd413
vbondarenko1 <vbondarenko@babbel.com>
2020-04-28 21:02:42 +0200

    simplistic patrolling of level implemented
    Level has 4 waypoints and the AI patrols them with a probability of changing the course of patrol

--- Unity Enemy AI/Assets/Scripts/EnemyAI.cs
+++ Unity Enemy AI/Assets/Scripts/EnemyAI.cs
@@ -6,36 +6,112 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    //Dictates whether the agent waits on each node
     [SerializeField]
-    Transform _destination;
+    bool _patrolWaiting;
 
+    //The total time we wait at each node
+    [SerializeField]
+    float _totalWaitTime = 3f;
+
+    //The probablility of sqitching direction
+    [SerializeField]
+    float _switchProbability = 0.2f;
+
+    //The list of all patrol nodes to visit
+    [SerializeField]
+    List<Waypoint> _patrolPoints;
+
+    //Private variables for base behaviour
     NavMeshAgent _navMeshAgent;
+    int _currentPatrolIndex;
+    bool _travelling;
+    bool _waiting;
+    bool _patrolForward;
+    float _waitTimer;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
+
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
 
         if (_navMeshAgent == null)
         {
-            Debug.LogError("The nav mesh component is not attached to " + gameObject.name);
+            Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
         }
-        else {
-            SetDestination();
+        else
+        {
+            if (_patrolPoints != null && _patrolPoints.Count >= 2)
+            {
+                _currentPatrolIndex = 0;
+                SetDestination();
+            }
+            else
+            {
+                Debug.Log("Insufficient patrol points for basic patrolling behaviour");
+            }
+        }
+    }
+
+    public void Update()
+    {
+        //Check if we're close to the destination
+
+        if (_travelling && _navMeshAgent.remainingDistance <= 1.0f)
+        {
+            _travelling = false;
+
+            //If we're going to wait, then wait.
+            if (_patrolWaiting)
+            {
+                _waiting = true;
+                _waitTimer = 0f;
+            }
+            else
+            {
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+
+        //If we're waiting
+        if (_waiting) {
+            _waitTimer += Time.deltaTime;
+
+            if (_waitTimer >= _totalWaitTime) {
+                _waiting = false;
+
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+    }
+
+    private void ChangePatrolPoint()
+    {
+        if (UnityEngine.Random.Range(0f, 1f) <= _switchProbability)
+        {
+            _patrolForward = !_patrolForward;
+        }
+
+        if (_patrolForward)
+        {
+            _currentPatrolIndex = (_currentPatrolIndex + 1) % _patrolPoints.Count;
+        }
+        else
+        {
+            if (--_currentPatrolIndex < 0) {
+                _currentPatrolIndex = _patrolPoints.Count - 1;
+            }
         }
     }
 
     private void SetDestination()
     {
-        if (_destination != null) {
-            Vector3 targetVector = _destination.transform.position;
+        if (_patrolPoints != null) {
+            Vector3 targetVector = _patrolPoints[_currentPatrolIndex].transform.position;
             _navMeshAgent.SetDestination(targetVector);
+            _travelling = true;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
